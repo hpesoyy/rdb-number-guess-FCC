8dddb74cf0830a80e73d2d03320ab9f6cffdf3b7
hpesoyy <yosephyilak640@gmail.com>
2025-08-24 07:14:20 -0400

    Get user input

--- number_guess.sh
+++ number_guess.sh
@@ -5,3 +5,19 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 # Ask for username
 echo "Enter your username:"
 read USERNAME
+
+# Check if username exists
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+if [[ -z $USER_ID ]]
+then
+  # Insert new user
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')" > /dev/null
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+#
